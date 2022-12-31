31222d1f6e744d24d49fc4dcdf2dc68b55ff6b45
Tanishq Baweja <tanishq.baweja001@gmail.com>
2022-12-31 20:48:01 +0530

    ignited-our-app

--- App.js
+++ App.js
@@ -1,3 +1,27 @@
+/***
+ *
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev and Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatble with older version of browser
+ * HTTPS on dev
+ * port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ *
+ * Transitive Dependencies
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading1 = React.createElement("h1", 
     {
         id: "title",
@@ -12,10 +36,11 @@ const heading1 = React.createElement("h1",
     const container = React.createElement("div", 
     {
         id: "container",
+        hello: "namaste" // we can declare any custom attributes as well => props in react
     }, 
     [heading1, heading2]);
 
-    // console.log(heading);
+    // console.log(heading1);
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
     // passing a react element inside the root
