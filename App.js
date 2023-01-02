2a19c6885988e9f4a3e70381fa3b1c401b42fdfc
Tanishq Baweja <tanishq.baweja001@gmail.com>
2023-01-02 08:24:44 +0530

    formatted App.js

--- App.js
+++ App.js
@@ -22,27 +22,33 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement("h1", 
-    {
-        id: "title",
-    }, 
-    "Heading1");
-    const heading2 = React.createElement("h2", 
-    {
-        id: "title",
-    }, 
-    "Heading2");
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "Heading1"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title",
+  },
+  "Heading2"
+);
 
-    const container = React.createElement("div", 
-    {
-        id: "container",
-        hello: "namaste" // we can declare any custom attributes as well => props in react
-    }, 
-    [heading1, heading2]);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+    hello: "namaste", // we can declare any custom attributes as well => props in react
+  },
+  [heading1, heading2]
+);
 
-    // console.log(heading1);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading1);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    // passing a react element inside the root
-    root.render(container);
-    // heading will only be rendered, all other things will be replaced/overwritten by this
+// passing a react element inside the root
+root.render(container);
+// heading will only be rendered, all other things will be replaced/overwritten by this
