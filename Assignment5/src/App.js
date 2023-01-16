866a1f8926f0add76f06d4440a1f18cbaa22fe98
Tanishq Baweja <tanishq.baweja001@gmail.com>
2023-01-16 21:20:33 +0530

    assignment-5 files

---
+++ Assignment5/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from './components/Footer'
+
+// ------------------ Composing Components ------------------------------->
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+// ------------------ Rendering ------------------------------->
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
