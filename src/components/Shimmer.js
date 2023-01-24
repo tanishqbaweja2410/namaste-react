48e3fd4874dd01baf20ff53665ecbaa39d817af8
Tanishq Baweja <tanishq.baweja001@gmail.com>
2023-01-24 17:29:16 +0530

    shimmer-updated

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,15 +1,16 @@
 const Shimmer = () => {
   return (
-    <div class="cardListShimmer">
-      {...Array(10).fill(
-        <div class="cardShimmer is-loading">
-          <div class="image"></div>
-          <div class="content">
-            <h2></h2>
-            <p></p>
-          </div>
-        </div>
-      )}
+    <div className="restaurantList">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
     </div>
   );
 };
