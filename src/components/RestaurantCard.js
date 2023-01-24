da977b646622e2df8a6a6334ed9ac827a5fb5778
Tanishq Baweja <tanishq.baweja001@gmail.com>
2023-01-24 17:29:50 +0530

    restCard-updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,33 +1,13 @@
 import { IMG_CDN_URL } from "../config";
 
-const RestaurantCard = ({ cloudinaryImageId, name, cuisines, avgRating, slaString, costForTwoString }) => {
+const RestaurantCard = ({ cloudinaryImageId, name, cuisines, avgRating }) => {
   return (
     <div className="card">
       <img src={IMG_CDN_URL + cloudinaryImageId} alt="" />
-      <h2 style={{"margin": "10px", "margin-left": 0}}>{name}</h2>
+      <h2>{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
-      {/* <h4>{avgRating} stars</h4> */}
-      <div class="_3Mn31">
-        <div class="_9uwBC wY0my">
-          <span class="icon-star _537e4">&nbsp;★</span>
-          <span>{avgRating}</span>
-        </div>
-        <div>•</div>
-        <div>{slaString}</div>
-        <div>•</div>
-        <div class="nVWSi">{costForTwoString}</div>
-      </div>
+      <h4>{avgRating} stars</h4>
     </div>
-    // <div className="card-inner-container">
-    //   <img className="card-image" src={IMG_CDN_URL + cloudinaryImageId} alt={name} />
-    //   <div className="card-info-container">
-    //     <h3 className="card-name">{name}</h3>
-    //     <p className="card-cuisines">{cuisines.join(", ")}</p>
-    //     <div className="card-rating-container">
-    //       <p className="card-rating-text">{avgRating}</p>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
